a3dad5748ae0198fdb16dcffe7b49311554a1c58
PabloQATesting <pabloalvesgarcia@gmail.com>
2023-09-22 10:39:13 +0100

    removing sql configs

--- generators/app/templates/cypress.config.js
+++ generators/app/templates/cypress.config.js
@@ -11,22 +11,6 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      on("task", {
-        async connectDB(query) {
-          const client = new Client({
-            database: "",
-            user: "",
-            password: "",
-            host: "",
-            port: 1234,
-            ssl: true
-          })
-          await client.connect()
-          const res = await client.query(query)
-          await client.end()
-          return res.rows
-        }
-      })
       const version = config.env.VERSION || 'stage'
       const envs = {
         stage: "google",
