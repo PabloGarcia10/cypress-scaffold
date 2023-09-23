b8cc9d46a77a9589bbd7c9cc57c4094cda3234f6
PabloQATesting <pabloalvesgarcia@gmail.com>
2023-09-23 13:04:10 +0100

    removing log

--- generators/app/index.js
+++ generators/app/index.js
@@ -35,12 +35,10 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.log('Writing commands.js')
     this.fs.copy(
       this.templatePath('cypress/support/commands.js'),
       this.destinationPath('cypress/support/commands.js')
     );
-    this.log('Writing e2e.js')
     this.fs.copy(
       this.templatePath('cypress/support/e2e.js'),
       this.destinationPath('cypress/support/e2e.js')
@@ -50,41 +48,35 @@ module.exports = class extends Generator {
       this.templatePath('cypress.config.js'),
       this.destinationPath('./cypress.config.js')
     )
-    this.log('Writing fixtures')
     this.fs.copy(
       this.templatePath('cypress/fixtures'),
       this.destinationPath('cypress/fixtures')
     );
     //Write api requests depending on the user answer 
     if (this.props.api !== false) {
-      this.log('Writing API tests example')
       this.fs.copy(
         this.templatePath('cypress/e2e/api'),
         this.destinationPath('cypress/e2e/api')
       );
-      this.log('Writing api requests')
       this.fs.copy(
         this.templatePath('cypress/support/api/requests'),
         this.destinationPath('cypress/support/api/requests')
       )
     }
-
+    
     //Write web elements and objects depending on the user answer 
     if (this.props.web !== false) {
-      this.log('Writing WEB tests example')
       this.fs.copy(
         this.templatePath('cypress/e2e/web'),
         this.destinationPath('cypress/e2e/web')
       );
-      this.log('Writing page elements')
       this.fs.copy(
         this.templatePath('cypress/support/web/pageElements'),
         this.destinationPath('cypress/support/web/pageEelements')
       );
-      this.log('Writing page objects')
       this.fs.copy(
-        this.templatePath('cypress/support/web/objects'),
-        this.destinationPath('cypress/support/web/objects')
+        this.templatePath('cypress/support/web/pageObjects'),
+        this.destinationPath('cypress/support/web/pageObjects')
       );
     }
   }
